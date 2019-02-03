a1a01cb81723ba58cba24288f6e309e5f1df4fc3
Rodrigo S. Cavalcanti <rcavalcanti@unito.com.br>
2019-02-03 11:48:40 -0200

    first commit

---
+++ Impinj/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Impinj
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.addressText = new System.Windows.Forms.TextBox();
+            this.LogTextArea = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(186, 12);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(75, 23);
+            this.ConnectButton.TabIndex = 1;
+            this.ConnectButton.Text = "Conectar";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // addressText
+            // 
+            this.addressText.Location = new System.Drawing.Point(80, 15);
+            this.addressText.Name = "addressText";
+            this.addressText.Size = new System.Drawing.Size(100, 20);
+            this.addressText.TabIndex = 2;
+            this.addressText.TextChanged += new System.EventHandler(this.address_TextChanged);
+            this.addressText.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.address_KeyPress);
+            // 
+            // LogTextArea
+            // 
+            this.LogTextArea.Location = new System.Drawing.Point(12, 51);
+            this.LogTextArea.Name = "LogTextArea";
+            this.LogTextArea.Size = new System.Drawing.Size(776, 387);
+            this.LogTextArea.TabIndex = 3;
+            this.LogTextArea.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Endereço";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LogTextArea);
+            this.Controls.Add(this.addressText);
+            this.Controls.Add(this.ConnectButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.TextBox addressText;
+        private System.Windows.Forms.RichTextBox LogTextArea;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Impinj/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Impinj.OctaneSdk;
+
+/*
+ * OctaneSDK Workbook (Examples) -> www.go-gddq.com/down/2012-06/12060311273105.pdf * 
+ *
+ */
+
+namespace Impinj
+{
+    public partial class Form1 : Form
+    {
+
+        // Maps the connected Impinj Readers addresses to to their instances
+        static Dictionary<string, ImpinjReader> readers = new Dictionary<string, ImpinjReader>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public bool ValidateIPv4(string ipString)
+        {
+            if (String.IsNullOrWhiteSpace(ipString))
+            {
+                return false;
+            }
+
+            string[] splitValues = ipString.Split('.');
+            if (splitValues.Length != 4)
+            {
+                return false;
+            }
+
+            byte tempForParsing;
+
+            return splitValues.All(r => byte.TryParse(r, out tempForParsing));
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void address_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void address_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                e.Handled = true;
+
+                Button button = this.Controls.Find("connectButton", true)[0] as Button;
+                button.PerformClick();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            // The sender can be the textbox (address) or the button (connect).
+            
+            TextBox addressText = this.Controls.Find("addressText", true)[0] as TextBox;
+            string address = addressText.Text; 
+            
+            if (!ValidateIPv4(address))
+            {
+                string message = "Endereço IPV4 inválido.";
+                string caption = "Erro";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result;
+
+                // Displays the MessageBox.
+                result = MessageBox.Show(message, caption, buttons);
+
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    // Closes the parent form.
+                    // this.Close();
+                    addressText.Focus();
+                    return;
+                }
+            }
+
+            try
+            {
+
+                ImpinjReader reader = new ImpinjReader();
+
+                reader.ConnectionLost += OnConnectionLost;
+
+                reader.ConnectionLost += new ImpinjReader.ConnectionLostHandler((ImpinjReader reader2) => {
+                    Console.Write("Connection lost");
+                });
+                
+                if (readers.ContainsKey(address))
+                {
+                    string message = "O leitor já está conectado.";
+                    string caption = "Erro";
+                    MessageBoxButtons buttons = MessageBoxButtons.OK;
+                    DialogResult result;
+
+                    // Displays the MessageBox.
+                    result = MessageBox.Show(message, caption, buttons);
+
+                    if (result == System.Windows.Forms.DialogResult.OK)
+                    {
+                        // Closes the parent form.
+                        // this.Close();
+                        addressText.Focus();
+                        return;
+                    }
+                }
+
+                reader.Connect(address);
+                readers.Add(address, reader);
+
+                Settings settings = reader.QueryDefaultSettings();
+                settings.ReaderMode = ReaderMode.AutoSetDenseReader;
+
+                ReportConfig report = settings.Report;
+                report.IncludeAntennaPortNumber = true;
+                report.IncludeFirstSeenTime = true;
+                report.IncludeLastSeenTime = true;
+                report.IncludeSeenCount = true;
+                report.Mode = ReportMode.Individual;
+
+                AntennaConfigGroup antennas = settings.Antennas;
+                antennas.DisableAll();
+
+                antennas.EnableById(new ushort[] { 1 });
+                AntennaConfig antennaConfig = antennas.GetAntenna(1);
+                antennaConfig.MaxRxSensitivity = true;
+                antennaConfig.RxSensitivityInDbm = -70;
+                antennaConfig.MaxTxPower = true;
+                antennaConfig.TxPowerInDbm = 20.0;
+
+                // Assign the TagsReported event handler.
+                // This specifies which method to call
+                // when tags reports are available.
+                reader.TagsReported += OnTagsReported;
+
+                // Apply the custom settings.
+                reader.ApplySettings(settings);
+                // Apply the default settings.
+                reader.ApplyDefaultSettings();
+
+                // Start reading.
+                reader.Start();
+            }
+            catch (OctaneSdkException ex)
+            {
+                Console.Write(ex.ToString());
+                return;
+            }
+
+            Control[] controls = this.Controls.Find("LogTextArea", true);
+            RichTextBox logs = controls[0] as RichTextBox;
+            logs.Invoke(new Action(() => {
+                addressText.Clear();
+                logs.AppendText("Leitor: " + address + " foi conectado com sucesso.\n");
+            }));
+ 
+        }
+
+        void OnTagsReported(ImpinjReader sender, TagReport report)
+        {
+            // This event handler is called asynchronously 
+            // when tag reports are available.
+            // Loop through each tag in the report 
+            // and print the data.
+            Control[] controls = this.Controls.Find("LogTextArea", true);
+            RichTextBox logs = controls[0] as RichTextBox;
+            foreach (Tag tag in report)
+            {
+                logs.Invoke(new Action(() => {
+                    logs.AppendText("EPC : " + tag.Epc.ToString() + " - " + sender.Address + ":" + tag.AntennaPortNumber + "\n");
+                    logs.SelectionStart = logs.Text.Length;
+                    logs.ScrollToCaret();
+                }));
+                
+                Console.WriteLine("EPC : {0} ", tag.Epc);
+            }
+        }
+
+        void OnConnectionLost(ImpinjReader sender)
+        {
+            Console.Write("Connetion Lost.");
+        }
+    }
+}
